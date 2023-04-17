f5aeb24ca595efe1a96ddd6e3704ab52438079d5
Josselin Milon <josselinmilon@MacBook-Pro-de-Josselin-2.local>
2023-04-17 16:57:50 +0200

    setting up env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "./firebase-creds";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGER_SENDING_ID,
+  appId: process.env.REACT_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
